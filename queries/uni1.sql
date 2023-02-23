94d6d620dcb57e79737ed152ec17febcf9e2cb1a
sasha <pausansa59@gmail.com>
2023-02-23 10:53:49 +0100

    Acabar uni1

--- queries/uni1.sql
+++ queries/uni1.sql
@@ -3,4 +3,7 @@ SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' AND telef
 SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' AND YEAR(fecha_nacimiento) = '1999';
 SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%F';
 SELECT nombre, id FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
-SELECT P.apellido1, P.apellido2, P.nombre, D.nombre AS 'Nombre Departamento'  FROM persona P INNER JOIN profesor Pr ON p.id = Pr.id_profesor INNER JOIN departamento D ON Pr.id_departamento=D.id;
+SELECT P.apellido1, P.apellido2, P.nombre, D.nombre AS 'Nombre Departamento'  FROM persona P INNER JOIN profesor Pr ON p.id = Pr.id_profesor INNER JOIN departamento D ON Pr.id_departamento=D.id ORDER BY P.apellido1, P.apellido2, P.nombre ASC;
+SELECT A.nombre, CE.anyo_inicio, CE.anyo_fin FROM persona P INNER JOIN alumno_se_matricula_asignatura ASA ON P.id = ASA.id_alumno INNER JOIN curso_escolar CE ON ASA.id_curso_escolar = CE.id INNER JOIN asignatura A ON ASA.id_asignatura = A.id  WHERE P.nif = '26902806M' AND P.tipo='alumno';
+SELECT DISTINCT D.nombre FROM profesor P INNER JOIN departamento D ON P.id_departamento = D.id INNER JOIN asignatura A ON A.id_profesor = P.id_profesor INNER JOIN grado G ON A.id_grado = G.id;
+
